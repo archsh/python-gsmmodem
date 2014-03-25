32699617932a2e1cb652ee2d9e24700920daf720
Mingcai SHEN <shenmc@dingrong.cn>
2014-03-25 11:20:28 -0700

    Updated example demo.

--- examples/sms_handler_demo.py
+++ examples/sms_handler_demo.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+# -*- coding: utf-8 -*-
 """\
 Demo: handle incoming SMS messages by replying to them
 
@@ -8,33 +8,42 @@ and the messages, then replies to the SMS by saying "thank you"
 """
 
 from __future__ import print_function
-
+import sys, datetime
 import logging
+sys.path.append('..')
 
-PORT = '/dev/ttyUSB2'
-BAUDRATE = 115200
-PIN = None # SIM card PIN (if any)
+PORT = 'COM4'
+BAUDRATE = 9600
+PIN = None  # SIM card PIN (if any)
 
 from gsmmodem.modem import GsmModem
+
 
 def handleSms(sms):
     print(u'== SMS message received ==\nFrom: {0}\nTime: {1}\nMessage:\n{2}\n'.format(sms.number, sms.time, sms.text))
     print('Replying to SMS...')
     sms.reply(u'SMS received: "{0}{1}"'.format(sms.text[:20], '...' if len(sms.text) > 20 else ''))
     print('SMS sent.\n')
-    
+
+
 def main():
-    print('Initializing modem...')
+    print('[%s] Initializing modem...' % datetime.datetime.now())
     # Uncomment the following line to see what the modem is doing:
-    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
+    logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=logging.DEBUG)
     modem = GsmModem(PORT, BAUDRATE, smsReceivedCallbackFunc=handleSms)
-    modem.smsTextMode = False 
+    modem.smsTextMode = False
     modem.connect(PIN)
-    print('Waiting for SMS message...')    
-    try:    
-        modem.rxThread.join(2**31) # Specify a (huge) timeout so that it essentially blocks indefinitely, but still receives CTRL+C interrupt signal
+    print('[%s] Initialized modem!' % datetime.datetime.now())
+    print('[%s] Sending Message...' % datetime.datetime.now())
+    modem.sendSms('18926025825', u'尊敬的用户，您好！欢饮使用KKBEAR！%s ' % datetime.datetime.now())
+    print('[%s] Waiting for SMS message...' % datetime.datetime.now())
+    try:
+        modem.rxThread.join(2 ** 31)
+        # Specify a (huge) timeout so that it essentially blocks indefinitely,
+        # but still receives CTRL+C interrupt signal
     finally:
-        modem.close();
+        modem.close()
+
 
 if __name__ == '__main__':
     main()
